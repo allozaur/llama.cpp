841b8a1886a852c8f6789d0a442031a75c31779d
Aleksander Grygier <aleksander.grygier@gmail.com>
2026-01-23 15:09:50 +0100

    feat: UI improvements

--- tools/server/webui/src/lib/components/app/chat/ChatForm/ChatForm.svelte
+++ tools/server/webui/src/lib/components/app/chat/ChatForm/ChatForm.svelte
@@ -304,12 +304,11 @@
 	/>
 
 	<div
-		class="flex-column relative min-h-[48px] items-center rounded-3xl py-2 pb-2.25 shadow-sm transition-all focus-within:shadow-md md:!py-3"
+			class="flex-column relative min-h-[48px] items-center rounded-3xl p-2 pb-2.25 shadow-sm transition-all focus-within:shadow-md md:!p-3"
 		onpaste={handlePaste}
 	>
 		<ChatFormTextarea
-			class="px-5 py-1.5 md:pt-0"
-			bind:this={textareaRef}
+				class="px-2 py-1 md:py-0"
 			bind:value={message}
 			onKeydown={handleKeydown}
 			{disabled}
